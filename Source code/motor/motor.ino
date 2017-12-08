81202d6903a973c6d0b6424ab599be567ea1dc19
NJU-RA <NJURobotAssociation@gmail.com>
2017-12-08 22:28:58 +0800

    code

--- Source code/motor/motor.ino
+++ Source code/motor/motor.ino
@@ -1,9 +1,10 @@
 /*---------------宏定义部分----------------*/
-#define SPEED_LEFT 150
-#define SPEED_RIGHT 150
+#define SPEED_LEFT 50
+#define SPEED_RIGHT 50
 
 /*--------------全局变量部分---------------*/
 int IN[5]={-1,3,5,6,9};
+int A5=5,A6=6;
 
 /*-------------子函数部分------------------*/
 
@@ -34,5 +35,36 @@ void motor(int speed_left,int speed_right)
     digitalWrite(IN[4],LOW);
     analogWrite(IN[3],-speed_right);
   }
+}
+
+/**
+  * 该函数通过模拟引脚读取输出的模拟量。
+  */
+void signal_read(void)
+{
+  int data1=0,data2=0;
+  data1=analogRead(A5);
+  data2=analogRead(A6);
+  Serial.println(data1);
+  Serial.println(data2);
+}
+
+
+void setup()
+{
+  pinMode(IN[1],OUTPUT);
+  pinMode(IN[2],OUTPUT);
+  pinMode(IN[3],OUTPUT);
+  pinMode(IN[4],OUTPUT);
+  pinMode(A5,INPUT);
+  pinMode(A6,INPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  motor(SPEED_LEFT,SPEED_RIGHT);
+  delay(100);
+  signal_read();
 }
 
---
+++ Source code/常用库函数例程/数字IO口/digitalRead()/code.ino
@@ -0,0 +1,17 @@
+//将引脚13设置为与引脚7相同的值，并声明为输入。
+
+int ledPin = 13; // LED connected to digital pin 13
+int inPin = 7;   // pushbutton connected to digital pin 7
+int val = 0;     // variable to store the read value
+
+void setup()
+{
+  pinMode(ledPin, OUTPUT);      // sets the digital pin 13 as output
+  pinMode(inPin, INPUT);      // sets the digital pin 7 as input
+}
+
+void loop()
+{
+  val = digitalRead(inPin);   // read the input pin
+  digitalWrite(ledPin, val);    // sets the LED to the button's value
+}
